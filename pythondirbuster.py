2cd57f5be7dcf4a5d46ee05b0da8669fb9341288
Lucas Fassi <96154450+sh4wty1@users.noreply.github.com>
2023-01-28 03:36:06 -0300

    Add files via upload

--- pythondirbuster.py
+++ pythondirbuster.py
@@ -1,36 +1,39 @@
-import requests;
+import requests
+from colored import fg
+abc = fg('purple_3')
+color = fg('green')
+color2 = fg('red')
 def Apresentation():    
     print("")
-    print("  _____       _   _                   _____  _      _               _   ")
-    print(" |  __ \     | | | |                 |  __ \(_)    | |             | |  ")
-    print(" | |__) |   _| |_| |__   ___  _ __   | |  | |_ _ __| |__  _   _ ___| |_ ")
-    print(" |  ___/ | | | __| '_ \ / _ \| '_ \  | |  | | | '__| '_ \| | | / __| __|")
-    print(" | |   | |_| | |_| | | | (_) | | | | | |__| | | |  | |_) | |_| \__ \ |_ ")
-    print(" |_|    \__, |\__|_| |_|\___/|_| |_| |_____/|_|_|  |_.__/ \__,_|___/\__|")
-    print("         __/ |                                                          ")
-    print("        |___/                                                           ")
+    print(abc + "  _____       _   _                   _____  _      _               _   ")
+    print(abc + " |  __ \     | | | |                 |  __ \(_)    | |             | |  ")
+    print(abc + " | |__) |   _| |_| |__   ___  _ __   | |  | |_ _ __| |__  _   _ ___| |_ ")
+    print(abc + " |  ___/ | | | __| '_ \ / _ \| '_ \  | |  | | | '__| '_ \| | | / __| __|")
+    print(abc + " | |   | |_| | |_| | | | (_) | | | | | |__| | | |  | |_) | |_| \__ \ |_ ")
+    print(abc + " |_|    \__, |\__|_| |_|\___/|_| |_| |_____/|_|_|  |_.__/ \__,_|___/\__|")
+    print(abc + "         __/ |                                                          ")
+    print(abc + "        |___/                                                           ")
     print("")
-    print("Coded by @nglshawty1 :)")
+    print(abc + "Coded by @nglshawty1 :)")
     print("")
-    print("Warning: You NEED to put the 'https://' part of the URL, otherwise, it will not work. Example:")
-    print("Incorrect: google.com")
-    print("Correct: https://google.com/")
-    print("This can take really a long time due to the huge wordlist, so be patient lol...")
+    print(color2 + "Warning: You NEED to put the 'https://' part of the URL, otherwise, it will not work. Example:")
+    print(color2 + "Incorrect: google.com")
+    print(color2 + "Correct: https://google.com/")
+    print(abc + "This can take really a long time due to the huge wordlist, so be patient lol...")
     print("")
 
 def Main():
-    Apresentation();
+    Apresentation()
     file = open("Subdomain.txt")
     lines = file.readlines()
     target = input('Type your URL -> ')
     for word in lines:
-        word = word.replace('\n', '');
+        word = word.replace('\n', '')
         r = requests.get(target+word)
         if 200 <= r.status_code <= 299:
-            print("########################")
-            print(word,"// status code:", r.status_code)
-            print("########################")
+            print(color + "########################")
+            print(color + word, "// status code:", r.status_code)
+            print(color + "########################")
         else:
-            print("Offline")
+            print(color2 + "Offline")
 Main()
-# Arrumar uma falha
